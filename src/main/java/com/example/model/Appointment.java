8d322249635c74f2afce6a8bd6b4a93ca44f918a
Thivya <thivya26sanji@gmail.com>
2026-08-18 21:28:14 +0530

    Package structure and configure MySQL database connection

--- src/main/java/com/example/model/Appointment.java
+++ src/main/java/com/example/model/Appointment.java
@@ -1,8 +1,15 @@
-package com.example.demo.model;
+package com.example.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "appointments")
--- src/main/java/com/example/model/Bill.java
+++ src/main/java/com/example/model/Bill.java
@@ -1,7 +1,14 @@
-package com.example.demo.model;
+package com.example.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "bills")
